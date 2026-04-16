1aab19948f37db54372e5ab20ede9655e25ac5c9
Miraj Mhaisuria <mirajmhaisuria@gmail.com>
2026-04-16 19:25:26 +1000

    fix: updated output syntax to use brackets for tflint

--- main.tf
+++ main.tf
@@ -171,5 +171,5 @@ resource "google_compute_external_vpn_gateway" "aws_gateway" {
 }
 # --- OUTPUTS ---
 output "firewall_public_ip" {
-  value = google_compute_instance.pan_fw.network_interface.0.access_config.0.nat_ip
+  value = google_compute_instance.pan_fw.network_interface[0].access_config[0].nat_ip
 }
